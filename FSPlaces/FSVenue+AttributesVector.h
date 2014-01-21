a018c4596c06529552bccab7a27ea758d380172c
nataliq <natalia.patsovska@Nataliyas-iMac.local>
2014-01-21 19:55:40 +0200

    Some algorithms added.

---
+++ FSPlaces/FSVenue+AttributesVector.h
@@ -0,0 +1,13 @@
+//
+//  FSVenue+AttributesVector.h
+//  FSPlaces
+//
+//  Created by Natalia Patsovska on 1/21/14.
+//  Copyright (c) 2014 MMAcademy. All rights reserved.
+//
+
+#import "FSVenue.h"
+
+@interface FSVenue (AttributesVector)
+
+@end
---
+++ FSPlaces/FSVenue+AttributesVector.m
@@ -0,0 +1,13 @@
+//
+//  FSVenue+AttributesVector.m
+//  FSPlaces
+//
+//  Created by Natalia Patsovska on 1/21/14.
+//  Copyright (c) 2014 MMAcademy. All rights reserved.
+//
+
+#import "FSVenue+AttributesVector.h"
+
+@implementation FSVenue (AttributesVector)
+
+@end
