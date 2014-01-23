3311f17c37dcd938f5da04e3778411a56a431bc6
Emil Marashliev <marashliev@gmail.com>
2014-01-23 16:52:19 +0200

    Add clustering functionallity.

---
+++ FSPlaces/FSCluster.h
@@ -0,0 +1,16 @@
+//
+//  FSCluster.h
+//  FSPlaces
+//
+//  Created by Emil Marashliev on 1/23/14.
+//  Copyright (c) 2014 MMAcademy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSCluster : NSObject
+
++ (NSArray *)clusterizeAndGetNearestVenues:(NSArray *)testVenues;
+
+
+@end
